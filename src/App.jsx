9649a27482d85efbb270fc09edfd9a4cf3fd74b8
akshargk <akshar.gk.s.113@kalvium.community>
2026-04-28 09:57:13 +0530

    feat: connect API and display country cards

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Header from "./components/Header"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
+import './styles/App.css'
 
 function App(){
   return(
