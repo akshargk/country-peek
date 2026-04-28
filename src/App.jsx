1f25c5ef13a4d6e89319e8a487545dcdae42cae2
akshargk <akshar.gk.s.113@kalvium.community>
2026-04-28 10:28:22 +0530

    wip: part 3 progress

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import {BrowserRouter,Routes,Route} from "react-router-dom";
 import Header from "./components/Header"
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound"
+import CountryPage from "./pages/CountryPage";
 
 function App(){
   return(
@@ -10,7 +11,7 @@ function App(){
         <main>
           <Routes>  
             <Route path="/" element={<Home/>}/>
-            <Route path="/country/:name" element={<div>Country Page</div>}/>
+            <Route path="/country/:code" element={<CountryPage />} />
             <Route path="/favourites" element={<div>Favourites Page</div>}/>
             <Route path="*" element={<NotFound/>}/>
           </Routes>
